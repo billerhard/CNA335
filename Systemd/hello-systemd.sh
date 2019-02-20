d4e1ed02e7a03c78c7fe0f94d7e97d593d7d1c68
bill <wherhard@student.rtc.edu>
2019-02-20 00:12:40 -0800

    Fixed so that it will actually be easy to find the result... but my user is hardcoded... #TODO

--- Systemd/hello-systemd.sh
+++ Systemd/hello-systemd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-touch ~/hello
-echo "Hello, this script is a service run by System.d on startup!" > ~/hello
-echo "Or, it will be when you type systemctl enable hello.service" >> ~/hello
-echo "You may also want to ensure you HAVE hello.service in the right place." >> ~/hello
-echo "You know what? It's kinda complicated." >> ~/hello
+touch /home/bill/hello
+echo "Hello, this script is a service run by System.d on startup!" > /home/bill/hello
+echo "Or, it will be when you type systemctl enable hello.service" >> /home/bill/hello
+echo "You may also want to ensure you HAVE hello.service in the right place." >> /home/bill/hello
+echo "You know what? It's kinda complicated." >> /home/bill/hello
