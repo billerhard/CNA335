1cb1a81c3a38f2c5ab7ab4d022d0948146bb0219
Bill <wherhard@student.rtc.edu>
2019-03-12 20:20:50 -0700

    Update configure.sh
    This doesn't work well on my ubuntu 18 desktop, comes pre-installed with a version of openssl that is incompatible? I think. hard to tell.

--- RTMP/configure.sh
+++ RTMP/configure.sh
@@ -2,13 +2,14 @@ sudo apt install build-essential
 sudo apt install libpcre3
 sudo apt install libpcre3-dev
 sudo apt install libssl-dev
+sudo apt install zlib1g-dev
 sudo apt install curl
 wget http://nginx.org/download/nginx-1.15.1.tar.gz
 wget https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/dev.zip
 tar -zxvf nginx-1.15.1.tar.gz
 unzip dev.zip
 cd nginx-1.15.1
-./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-dev
+./configure --with-http_ssl_module --with-openssl=/usr/lib/ssl --add-module=../nginx-rtmp-module-dev
 make
 sudo make install
 sudo /usr/local/nginx/sbin/nginx
